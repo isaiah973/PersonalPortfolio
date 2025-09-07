5e462d7791b4cade0868b5f291caf1d4ea3b4d77
isaiah973 <adeolaisaiah01@gmail.com>
2025-09-07 23:15:10 +0100

    second commit

--- src/Components/FloatNavbar.jsx
+++ src/Components/FloatNavbar.jsx
@@ -7,7 +7,7 @@ import { FaFileAlt } from "react-icons/fa";
 const FloatNavbar = () => {
   return (
     <div>
-      <div className="fixed top-[90vh] right-[40%] flex  space-x-3 items-center justify-center h-16 px-4 bg-primary border-[2px] border-teal-800 hover:border-teal-800 hover:bg-primary transition duration-300 rounded-[50px] opacity-75">
+      <div className="fixed bottom-[5vh] left-0 right-0 m-auto flex items-center justify-center h-[10vh] px-4 bg-primary border-[2px] border-teal-800 hover:border-teal-800 hover:bg-primary transition duration-300 rounded-[50px] opacity-75 w-[fit-content]">
         
         <div className="h-[50px] w-[50px] flex items-center justify-center hover:bg-black transition duration-300 rounded-[50%]"><AiFillHome className="text-2xl text-gray-400"/></div>
 
--- src/Components/HeroSection.jsx
+++ src/Components/HeroSection.jsx
@@ -9,18 +9,18 @@ const HeroSection = () => {
   return (
     <div>
       <FloatNavbar/>
-      <div className="w-full h-[100vh] bg-primary flex items-center justify-center pb-36">
+      <div className="w-full h-[100vh] bg-primary px-2">
         {/* <Navbar /> */}
-        <div className="w-full font-Poppins flex items-center justify-center space-x-64">
+        <div className=" w-full font-Poppins flex items-center justify-center lg:space-x-64 pt-[40%] lg:pt-[12%]">
 
-          <div className=" h-72 pl-4">
-            <div>
+          <div className=" h-72 flex flex-col align-center justify-center lg:items-start">
+            <div className="flex flex-col align-center text-center lg:text-left">
               <p className=" text-text-green">Hello, I'm</p>
-              <h1 className="text-7xl font-bold text-text-white flex flex-col"><span className="font-light">ADEOLA</span><span>OMOTOYINBO</span></h1>
-              <p className="text-5xl font-thin text-text-green">Full-Stack Developer</p>
+              <h1 className="text-3xl lg:text-7xl font-bold text-text-white flex flex-col"><span className="font-light">ADEOLA</span><span>OMOTOYINBO</span></h1>
+              <p className="text-3xl font-thin text-text-green lg:text-5xl">Full-Stack Developer</p>
             </div>
 
-            <div className="mt-12">
+            <div className="mt-8 flex align-center justify-center">
               <button className="py-3 bg-primary border-[1px] border-text-green text-text-white px-3 text-shadow-lg mr-6 rounded-[2px] hover:bg-text-green transition duration-300 hover:text-primary">Download CV</button>
               <button className="py-3 bg-text-green text-text-white border-[1px] border-text-green px-3 text-shadow-lg rounded-[2px] hover:bg-primary transition duration-300 hover:text-white hover:border-text-green hover:border-[1px]">Let's Talk</button>
             </div>
